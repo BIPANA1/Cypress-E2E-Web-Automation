7aa6b8de6407213d6e7e2bf759e0f3ab85d75456
Bipana <bipanashrestha435@gmail.com>
2025-03-27 18:26:55 +0545

    select & unselect:checkbox & radio button

---
+++ cypress/e2e/RadioButton&Checkbox.cy.js
@@ -0,0 +1,43 @@
+describe("Check UI Element", () =>{
+    // it("Checking Radio Buttons", () =>{
+    //     cy.visit("https://testautomationpractice.blogspot.com/")
+        
+    //     //visibility of radio button
+    //     cy.get('#male').should('be.visible')
+    //     cy.get('#female').should('be.visible')
+
+    //     //selecting radio buttons
+    //     cy.get('#male').check().should('be.checked')
+    //     cy.get('#female').should('not.be.checked')
+
+
+    // })
+
+    //Visibility of element
+    it("Checking Checkbox Buttons", () =>{
+        cy.visit("https://testautomationpractice.blogspot.com/")
+        cy.get("input#monday").should("be.visible")
+
+        //selecting single checkboxes
+        cy.get("input#monday").check().should("be.checked")
+
+        //unchecking checkbox
+        cy.get("input#monday").uncheck().should("not.be.checked")
+
+        // select all checkbox at once
+        cy.get("input.form-check-input[type='checkbox']").check().should("be.checked")
+
+        //Unselect all checkbox at once
+        cy.get("input.form-check-input[type='checkbox']").uncheck().should("not.be.checked")
+
+
+        //Select 1st checkbox
+        cy.get("input.form-check-input[type='checkbox']").first().check()
+        cy.get("input.form-check-input[type='checkbox']").last().check()
+
+
+        //git commit -m "select & unselect:checkbox & radio button "
+      
+
+    })
+})
